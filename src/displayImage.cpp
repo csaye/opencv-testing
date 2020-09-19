5a4238678ed0d66c8465d7aaecf73fe35c2eb0f1
Cooper Saye <coopersaye@gmail.com>
2020-09-18 19:04:03 -0700

    Set up separate program files.

--- src/displayImage.cpp
+++ src/displayImage.cpp
@@ -1,3 +1,5 @@
+// g++ -std=c++17 `pkg-config --cflags --libs opencv4` main.cpp -o DisplayImage
+
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <iostream>
@@ -14,7 +16,7 @@ int main( int argc, char** argv )
     }
 
     Mat image;
-    image = imread(argv[1], CV_LOAD_IMAGE_COLOR);   // Read the file
+    image = imread(argv[1], IMREAD_COLOR);   // Read the file
 
     if(! image.data )                              // Check for invalid input
     {
